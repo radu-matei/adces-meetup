b9b015021370748052b3986238add45f4f9c921b
Radu Matei <matei.radu94@gmail.com>
2016-11-08 14:27:39 +0200

    Added basic project

---
+++ src/configuration/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Hosting;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var host = new WebHostBuilder()
+            .UseKestrel()
+            .UseStartup<Startup>()
+            .Build();
+
+        host.Run();
+    }
+}
