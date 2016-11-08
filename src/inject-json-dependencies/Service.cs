20fd9c15554300b71fb454046dec69573fd78839
Radu Matei <matei.radu94@gmail.com>
2016-11-08 17:44:59 +0200

    Added Service class

---
+++ src/inject-json-dependencies/Service.cs
@@ -0,0 +1,13 @@
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+public class Service
+{
+    public string ServiceType { get; set; }
+
+    public string ImplementationType { get;set; }
+
+    [JsonConverter(typeof(StringEnumConverter))]
+    public ServiceLifetime Lifetime { get; set; }
+}
